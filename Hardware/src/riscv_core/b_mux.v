6d63507945b122d32b4a082f2de7a2b10e7de700
XianceXu <natecece0114@berkeley.edu>
2025-11-16 12:51:34 +0800

    Create b_mux.v

---
+++ Hardware/src/riscv_core/b_mux.v
@@ -0,0 +1,14 @@
+module b_mux(reg_file_data_b, imm_gen_out, alu_forward, dmem_foward, b_sel, b_out);
+    input [31:0] reg_file_data_b, imm_gen_out, alu_forward, dmem_foward;
+    input [1:0] b_sel;
+    output reg [31:0] b_out;
+
+    always @(*) begin
+        case(b_sel)
+            2'd0: b_out = reg_file_data_a;
+            2'd1: b_out = imm_gen_out;
+            2'd2: b_out = alu_forward;
+            2'd3: b_out = dmem_foward;
+        endcase
+    end
+endmodule
