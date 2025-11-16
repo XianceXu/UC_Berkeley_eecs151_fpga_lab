a4677481cf96a6f691dd8aca1d829275a474d890
XianceXu <natecece0114@berkeley.edu>
2025-11-16 13:01:53 +0800

    Create simple_register.v

---
+++ Hardware/src/riscv_core/simple_register.v
@@ -0,0 +1,9 @@
+module simple_register(q, d, clk);
+  parameter N = 1;
+  output reg [N-1:0] q;
+  input [N-1:0]      d;
+  input 	     clk;
+  initial q = {N{1'b0}};
+  always @(posedge clk)
+    q <= d;
+endmodule // REGISTER
